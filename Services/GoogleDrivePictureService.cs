7f3e0162953b25110dbb78b81e95a08dde2ef1af
Utenkov Vladimir <vladimir.utenkov@gmail.com>
2020-06-05 09:37:05 +0300

    picture service

---
+++ Services/GoogleDrivePictureService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_vue_app.Services
+{
+    public class GoogleDrivePictureService : IPictureService
+    {
+        private string _pathPattern = "https://drive.google.com/uc?export=view&id=1X1ChJjhZkvnBGP4De8Gf5MnsHleaT6Qy";
+
+        public byte GetPicture()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
