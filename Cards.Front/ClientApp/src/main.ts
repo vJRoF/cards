3db0e877e9db359ca20c775b2268df47c8e079a1
Владимир Тимофеев <VlAlTimofeev@beeline.ru>
2020-07-04 15:56:04 +0300

    убрал вызовы из главного компонента

--- Cards.Front/ClientApp/src/main.ts
+++ Cards.Front/ClientApp/src/main.ts
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { CardsClient } from './cardsclient'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
-  mounted(){
-    const cc = new CardsClient();
-    cc.picture(1, "ddd")
-      .then(_ => alert('ура)))'))
-      .catch(_ => alert('пиздец((('));
-  }
+  render: h => h(App)
 }).$mount('#app')
