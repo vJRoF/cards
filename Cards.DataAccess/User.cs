2d36bb73a2d18dd166e498435464b31257656113
Владимир Тимофеев <VlAlTimofeev@beeline.ru>
2020-07-04 17:22:15 +0300

    fix migrations

--- Cards.DataAccess/User.cs
+++ Cards.DataAccess/User.cs
@@ -1,7 +1,8 @@
-﻿namespace Cards.DataAccess.Migrations
+﻿namespace Cards.DataAccess
 {
     public class User
     {
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
