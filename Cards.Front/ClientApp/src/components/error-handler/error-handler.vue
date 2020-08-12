b9e9ede8962a0c383f6a8b60161a769e334ffcb6
Владимир Тимофеев <VlAlTimofeev@beeline.ru>
2020-08-12 14:42:00 +0300

    error handler

---
+++ Cards.Front/ClientApp/src/components/error-handler/error-handler.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-dialog v-model="dialog" max-width="290">
+      <v-card>
+          <v-card-title>
+              Ошибка
+          </v-card-title>
+          <v-card-text>
+              {{message}}
+          </v-card-text>
+          <v-card-actions>
+              <v-btn
+                @click="dismiss">
+                Увы
+              </v-btn>
+          </v-card-actions>
+      </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class ErrorHandler extends Vue {
+    dialog = false;
+    message = '';
+    
+    display(message: string) {
+        this.message = message;
+        this.dialog = true;
+    }
+
+    dismiss() {
+        this.dialog = false;
+    }
+}
+</script>
--- Cards.Front/ClientApp/src/components/session-create/session-create.vue
+++ Cards.Front/ClientApp/src/components/session-create/session-create.vue
@@ -1,35 +1,63 @@
-<template src="./session-create.html">
+<template>
+  <v-main>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="8">
+          <v-text-field label="Имя сеанса" v-model="sessionName"></v-text-field>
+        </v-col>
+        <v-col>
+          <v-btn v-on:click="createSession">Создать</v-btn>
+        </v-col>
+      </v-row>
+      <!-- <v-row>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :options.sync="options"
+          :server-items-length="totalDesserts"
+          :loading="loading"
+          class="elevation-1"
+        ></v-data-table>
+      </v-row>-->
+    </v-container>
+    <error-handler ref="errorHandler"/>
+  </v-main>
 </template>
-<style scoped src="./session-create.css">
-</style>
 <script lang="ts">
-
-import Vue from 'vue'
-import {VContainer, VRow, VCol} from 'vuetify/lib'
-import {SessionsClient, IConfig} from "../../client/api-client"
+import Vue from "vue";
+import { VContainer, VRow, VCol } from "vuetify/lib";
+import { SessionsClient, IConfig, SessionModel } from "../../client/api-client";
+import ErrorHandler from "../error-handler/error-handler.vue";
 
 export default Vue.extend({
-    name: 'session-create',
-    components:{
-        VContainer,
-        VRow,
-        VCol
-    },
-    data() {
-        return {
-            sessionsClient: new SessionsClient(new IConfig("dlkdfklsl44kmrf")),
-            sessionName: "",
-            pageSize: 10
-        }
-    },
-    methods: {
-        async createSession() {
+  name: "session-create",
+  components: {
+    VContainer,
+    VRow,
+    VCol,
+    ErrorHandler,
+  },
+  data() {
+    return {
+      sessions: new Array<SessionModel>(),
+      sessionsClient: new SessionsClient(new IConfig("dlkdfklsl44kmrf")),
+      sessionName: "",
+      pageSize: 10,
+    };
+  },
+  methods: {
+    async createSession() {
+        try {
             console.log(`sessionName is [${this.sessionName}]`);
             await this.sessionsClient.create(this.sessionName);
-        },
-        async listSessions(){
-
         }
-    }
-})
+        catch(err) {
+            (this.$refs.errorHandler as ErrorHandler).display(err)
+        }
+    },
+    async listSessions() {
+      this.sessions = await this.sessionsClient.list(this.pageSize, 0);
+    },
+  },
+});
 </script>
